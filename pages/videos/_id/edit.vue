e578e7f356e260e751b521133de108931d7d654f
Guscosta88 <guslfcosta@gmail.com>
2022-06-03 16:43:20 +1000

    Dinamic pages added

---
+++ pages/videos/_id/edit.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1> Editing {{ video.name }} </h1>
+        <nuxt-link :to="`/videos/${video.id}`">
+        <button>Go Back To Show</button>
+        </nuxt-link>
+    </div>
+</template>
+<script>
+export default {
+    props: ['video']
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
---
+++ pages/videos/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        We are in Video {{ video.name }}.
+        <nuxt-link :to="`/videos/${video.id}/edit`">
+        <button>Edit</button>
+        </nuxt-link>
+    </div>
+</template>
+<script>
+export default {
+    props: ['video']
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
